f9a6abb6665a13800025982b572ce9a5a5bc2cfa
Yassine Yazidi <104203113+yazidiyassine@users.noreply.github.com>
2023-05-03 12:52:51 +0100

    adding custom Logging + logback.xml file

--- src/main/java/com/sms/config/ProjectSecurityConfig.java
+++ src/main/java/com/sms/config/ProjectSecurityConfig.java
@@ -20,7 +20,8 @@ public class ProjectSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().ignoringRequestMatchers("/saveMsg").ignoringRequestMatchers("/api/**").
+        http.csrf().ignoringRequestMatchers("/saveMsg").ignoringRequestMatchers("/api/**")
+                .ignoringRequestMatchers("/data-api/**").
                 ignoringRequestMatchers("/public").and().
                 authorizeRequests().requestMatchers("/home").permitAll()
                 .requestMatchers("/displayMessages").hasRole("ADMIN")
@@ -37,6 +38,7 @@ public class ProjectSecurityConfig {
                 .requestMatchers("/dashboard").authenticated()
                 .requestMatchers("/courses").authenticated()
                 .requestMatchers("/api/**").authenticated()
+                .requestMatchers("/data-api/**").authenticated()
                 .and()
                 .formLogin().loginPage("/login")
                 .defaultSuccessUrl("/dashboard")
--- src/main/java/com/sms/repository/CoursesRepository.java
+++ src/main/java/com/sms/repository/CoursesRepository.java
@@ -2,11 +2,14 @@ package com.sms.repository;
 
 import com.sms.model.Courses;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RepositoryRestResource(path = "courses")
+//@RepositoryRestResource(exported = false)
 public interface CoursesRepository extends JpaRepository<Courses, Integer> {
 
 
